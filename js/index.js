e397838f1086cc65411ec15473d526b042079e43
oxyflour <a6905688@gmail.com>
2014-02-04 11:06:37 +0800

    use css transformation for animation instead

--- js/index.js
+++ js/index.js
@@ -424,7 +424,7 @@ app.directive('imgSrc', function() {
 })
 app.directive('anyMove', function() {
 	return function (scope, elem, attrs, ctrl) {
-		elem.bind("scroll touchstart mousedown", function(e) {
+		elem.bind("scroll touchstart touchcancel mousedown", function(e) {
 			scope.$eval(attrs.anyMove, { $event: e });
 			scope.$apply();
 		})
@@ -794,6 +794,10 @@ app.controller('main', function($scope, $location, $http, $timeout) {
 		reload: function() { location.reload(); }
 	}
 
+	$scope.$watch('tool.show', function(v, v0) {
+		if (v) $tool.showing = v;
+	});
+
 	$scope.$watch('list.url+list.total', function(v) {
 		var ls = $scope.list, pl = $scope.playlists
 		if (!v || !ls || ls.total === undefined) return;
@@ -835,4 +839,25 @@ app.controller('main', function($scope, $location, $http, $timeout) {
 		if ($audio.state == 'play')
 			return 'Track will stop if you leave.'
 	})
+
+	/*
+	function toggleFullScreen() {
+	  var doc = window.document;
+	  var docEl = doc.documentElement;
+
+	  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen;
+	  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen;
+
+	  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement) {
+		requestFullScreen.call(docEl);
+	  }
+	  else {
+		cancelFullScreen.call(doc);
+	  }
+	}
+
+	$(document).bind('touchstart', function(e) {
+		toggleFullScreen();
+	});
+	*/
 })
